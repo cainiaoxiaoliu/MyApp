f3d83da209f42c2248b2f616992364c73d080759
lyg <1049264172@qq.com>
2016-08-24 09:00:36 +0800

    MyApp

--- MyApp/src/com/lyg/Hello.java
+++ MyApp/src/com/lyg/Hello.java
@@ -7,13 +7,6 @@ import java.util.*;
  */
 public class Hello {
     public static void main(String[] args){
-       /*MyArrayList<String> list=new MyArrayList<String>();
-        List<String> str=new ArrayList<String>();
-        for(int i=0;i<5;i++){
-            list.add(i+",");
-        }
-        for(String str:list){
-            System.out.println(str);
-        }*/
+        System.out.println("Hello World");
     }
 }
